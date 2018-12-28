c7a4bd7282510b3a06611d70f4f03312aa3a8647
John <a0972199950@gmail.com>
2018-12-28 20:57:46 +0800

    更改firebase config，不使用process.env

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,21 @@
 import * as firebase from "firebase";
 
+// const config = {
+//     apiKey: process.env.FIREBASE_API_KEY,
+//     authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+//     databaseURL: process.env.FIREBASE_DATABASE_URL,
+//     projectId: process.env.FIREBASE_PROJECT_ID,
+//     storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+//     messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
+// };
+
 const config = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.FIREBASE_DATABASE_URL,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
+    apiKey: "AIzaSyBdutgaG_B9eC1ZlgYYxcy5aNW2dR2HLdQ",
+    authDomain: "expensify-practice.firebaseapp.com",
+    databaseURL: "https://expensify-practice.firebaseio.com",
+    projectId: "expensify-practice",
+    storageBucket: "expensify-practice.appspot.com",
+    messagingSenderId: "574930881559"
 };
 
 
